c58233dad342bc2f6e8aa8051552257eecbf9664
akivalencia <adilene.valencias@gmail.com>
2021-02-11 10:40:22 -0800

    just committing what i have so far

--- logic.js
+++ logic.js
@@ -1,16 +1,60 @@
+// document.getElementsByClassName("change").style.color = 'purple';
+//selecting the element 
+const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+const myTextInput= document.getElementById('myTextInput');
+
+//adding event and what to do 
+myButton.addEventListener('click', () => {
+    myHeading.style.color = myTextInput.value;
+});
+
+
+
+
+// function changeText() {
+//     // document.getElementById("change").style.color = 'purple';
+//     var color = document.getElementById("change").style.color = 'purple';
+//     // id.innerHTML = "ooops!";
+//   }
+
+  
+//     // Note that the listeners in this case are |this|, not this.handleEvent
+//     element.addEventListener('click', this, false);
+//     element.addEventListener('dblclick', this, false);
+  
+//     // You can properly remove the listeners this way:
+//     element.removeEventListener('click', this, false);
+//     element.removeEventListener('dblclick', this, false);
+//   }
+//   const s = new ColorChangeButton(document.body);
+
+
+
+
 function coinFlip(){
    let coin_side= ['heads', 'tails'];
    var answer= coin_side[Math.floor(Math.random() * coin_side.length)];
    //play ! there's an actual prompt
-   if (answer== 'heads') {
-       console.log(`the answer is ${answer}`)
-
-   }
-   else {console.log (`the answer is ${answer}`)}
+       return (`it's ${answer}`);
 }
+//testing coinFlip()
+//coinFlip();
+//coinFlip();
+//coinFlip();
 
-coinFlip();
-coinFlip();
-coinFlip();
-coinFlip();
+//call the coinFlip() as many times as user wants 
+//since instructions say will allow user to flip as many coins as wants 
+
+function playTimes(num){
+   for (i=1; i<=num; i++){
+       console.log(coinFlip());
+    }
+}
+playTimes(2);
+
+
+
+
+
 //undefined bc nothing returned 
